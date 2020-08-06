3ea781c37fda39d5f576c104184277724df341f3
FluffyFu <fluffyfu400@gmail.com>
2020-08-06 10:36:43 -0400

    finished tree_height.py.

--- week1_basic_data_structures/2_tree_height/test_tree_height_cases.py
+++ week1_basic_data_structures/2_tree_height/test_tree_height_cases.py
@@ -1,6 +1,8 @@
+import sys
+import threading
 from tree_height import build_tree, BFS, cal_tree_height, compute_height
-import pudb
 import glob
+
 
 files = glob.glob('tests/*')
 input_files = sorted([f for f in files if not 'a' in f])
@@ -8,17 +10,24 @@ result_files = sorted([f for f in files if 'a' in f])
 
 print('total number of test cases: ', len(input_files))
 
-counter = 1
-for input_name, result_name in zip(input_files, result_files):
-    with open(input_name) as f:
-        n = int(f.readline())
-        parents = [int(val) for val in f.readline().split()]
 
-    with open(result_name) as f:
-        truth = int(f.readline())
+def main():
+    counter = 1
+    for input_name, result_name in zip(input_files, result_files):
+        with open(input_name) as f:
+            n = int(f.readline())
+            parents = [int(val) for val in f.readline().split()]
 
-    assert compute_height(n, parents) == truth
+        with open(result_name) as f:
+            truth = int(f.readline())
 
-    print('finished test ', counter)
+        assert compute_height(n, parents) == truth
 
-    counter += 1
+        print('finished test ', counter)
+
+        counter += 1
+
+
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
