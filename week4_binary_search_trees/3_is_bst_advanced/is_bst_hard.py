6cac359a6962522b730e1edb8fe721b7aeb06f05
FluffyFu <fluffyfu400@gmail.com>
2020-08-19 11:41:59 -0400

    finished problem 3 week 4.

--- week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
+++ week4_binary_search_trees/3_is_bst_advanced/is_bst_hard.py
@@ -8,7 +8,106 @@ sys.setrecursionlimit(10**7)  # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
 
+class Solver:
+    """
+    Check if the given binary tree satisfies the following requirements:
+        1. The left child is smaller than the root.
+        2. The right child is equal or greater than the root.
+
+    This implementation uses a bottom-up approach, the tree is a legitimate BST if:
+        - its left and right subtree are legitimate BST
+        - the max value in the left substree is smaller than the parent.
+        - the min value in the right substree is greater or equal to the parent.
+    """
+    MIN = int(-10E16)
+    MAX = int(10E16)
+
+    def __init__(self, n: int, tree: List[List[int]]) -> None:
+        self._n = n
+        self._keys = [0 for _ in range(n)]
+        self._left = [-1 for _ in range(n)]
+        self._right = [-1 for _ in range(n)]
+        self._max_vals = [None for _ in range(n)]  # the max of subtree-i
+        self._min_vals = [None for _ in range(n)]  # the min of subtree-i
+
+        for i, item in enumerate(tree):
+            self._keys[i] = item[0]
+            self._left[i] = item[1]
+            self._right[i] = item[2]
+
+    def is_bst(self) -> bool:
+        if self._n == 0:
+            return True
+        return self._is_valid(0)
+
+    def _is_valid(self, root: int) -> bool:
+        """
+        Internal function to check if the binary tree complies the requirements.
+        """
+        if root == -1:
+            return True
+
+        is_valid_left = self._is_valid(self._left[root])
+        if not is_valid_left:
+            return False
+        is_valid_right = self._is_valid(self._right[root])
+        if not is_valid_right:
+            return False
+
+        key_val = self._keys[root]
+        left_max = self._max(self._left[root])
+        if key_val <= left_max:
+            return False
+        right_min = self._min(self._right[root])
+        if key_val > right_min:
+            return False
+
+        return True
+
+    def _max(self, root: int) -> int:
+        """
+        Find the max key in the tree.
+        """
+        if root == -1:
+            return self.MIN
+
+        # use cached value if it has been computed.
+        # this should come after root == -1 determination,
+        # otherwise, -1 index would cause unexpected result
+        # because that's just return the last element.
+        if self._max_vals[root]:
+            return self._max_vals[root]
+
+        key_val = self._keys[root]
+        left_max = self._max(self._left[root])
+        right_max = self._max(self._right[root])
+
+        self._max_vals[root] = max([key_val, left_max, right_max])
+        return self._max_vals[root]
+
+    def _min(self, root: int) -> int:
+        """
+        Find the min key in the tree.
+        """
+        if root == -1:
+            return self.MAX
+
+        if self._min_vals[root]:
+            return self._min_vals[root]
+
+        key_val = self._keys[root]
+        left_min = self._min(self._left[root])
+        right_min = self._min(self._right[root])
+
+        self._min_vals[root] = min([key_val, left_min, right_min])
+        return self._min_vals[root]
+
+
 class Solver2:
+    """
+    Top-down approach. As we go down the tree, we narrow down the legitimate value range of the
+    substrees. If any of the subtree violates the legitimate range, we simply return False.
+    """
     MIN = int(-10E16)
     MAX = int(10E16)
 
@@ -70,90 +169,12 @@ class Solver2:
             self._right[root], key_val, high, False)
 
 
-class Solver:
-    """
-    Check if the given binary tree satisfies the following requirements:
-        1. The left child is smaller than the root.
-        2. The right child is equal or greater than the root.
-    """
-    MIN = int(-10E16)
-    MAX = int(10E16)
-
-    def __init__(self, n: int, tree: List[List[int]]) -> None:
-        self._n = n
-        self._keys = [0 for _ in range(n)]
-        self._left = [-1 for _ in range(n)]
-        self._right = [-1 for _ in range(n)]
-        self._max_vals = [self.MIN for _ in range(n)]  # the max of subtree-i
-        self._min_vals = [self.MAX for _ in range(n)]  # the min of subtree-i
-
-        for i, item in enumerate(tree):
-            self._keys[i] = item[0]
-            self._left[i] = item[1]
-            self._right[i] = item[2]
-
-    def is_bst(self) -> bool:
-        if self._n == 0:
-            return True
-        return self._is_valid(0)
-
-    def _is_valid(self, root: int) -> bool:
-        """
-        Internal function to check if the binary tree complies the requirements.
-        """
-        if root == -1:
-            return True
-
-        is_valid_left = self._is_valid(self._left[root])
-        if not is_valid_left:
-            return False
-        is_valid_right = self._is_valid(self._right[root])
-        if not is_valid_right:
-            return False
-
-        key_val = self._keys[root]
-        left_max = self._max(self._left[root])
-        if key_val <= left_max:
-            return False
-        right_min = self._min(self._right[root])
-        if key_val > right_min:
-            return False
-
-        return True
-
-    def _max(self, root: int) -> int:
-        if self._max_vals[root] != self.MIN:
-            return self._max_vals[root]
-
-        if root == -1:
-            return self.MIN
-        key_val = self._keys[root]
-        left_max = self._max(self._left[root])
-        right_max = self._max(self._right[root])
-
-        self._max_vals[root] = max([key_val, left_max, right_max])
-        return self._max_vals[root]
-
-    def _min(self, root: int) -> int:
-        if self._min_vals[root] != self.MAX:
-            return self._min_vals[root]
-
-        if root == -1:
-            return self.MAX
-        key_val = self._keys[root]
-        left_min = self._min(self._left[root])
-        right_min = self._min(self._right[root])
-
-        self._min_vals[root] = min([key_val, left_min, right_min])
-        return self._min_vals[root]
-
-
 def main():
     nodes = int(sys.stdin.readline().strip())
     tree = []
     for i in range(nodes):
         tree.append(list(map(int, sys.stdin.readline().strip().split())))
-    solver = Solver2(nodes, tree)
+    solver = Solver(nodes, tree)
     if solver.is_bst():
         print("CORRECT")
     else:
--- week4_binary_search_trees/3_is_bst_advanced/test_is_bst_hard.py
+++ week4_binary_search_trees/3_is_bst_advanced/test_is_bst_hard.py
@@ -1,9 +1,10 @@
 from is_bst_hard import Solver, Solver2
 import pudb
+from binarytree import tree
+from collections import deque
 
 
 def test_solver():
-    # pudb.set_trace()
     n = 3
     tree = [
         [2, 1, 2],
@@ -85,6 +86,16 @@ def test_solver():
     ]
     solver = Solver(n, tree)
     assert solver.is_bst() == False
+
+    # pudb.set_trace()
+    n = 3
+    tree = [
+        [5, 1, -1],
+        [1, -1, 2],
+        [2, -1, -1]
+    ]
+    solver = Solver(n, tree)
+    assert solver.is_bst() == True
 
 
 def test_solver2():
@@ -169,3 +180,61 @@ def test_solver2():
     ]
     solver = Solver2(n, tree)
     assert solver.is_bst() == False
+
+    n = 3
+    tree = [
+        [5, 1, -1],
+        [1, -1, 2],
+        [2, -1, -1]
+    ]
+    solver = Solver2(n, tree)
+    assert solver.is_bst() == True
+
+
+def build_map_key_to_index(root):
+    vals = root.values
+    vals = [v for v in vals if v != None]
+    mapping = {v: i for i, v in enumerate(vals)}
+    return mapping
+
+
+def convert_tree_format(root):
+
+    mapping = build_map_key_to_index(root)
+
+    q = deque()
+    q.append(root)
+    res = []
+
+    while len(q) > 0:
+        node = q.popleft()
+        val = node.val
+        if node.left:
+            left_index = mapping[node.left.val]
+            q.append(node.left)
+        else:
+            left_index = -1
+
+        if node.right:
+            right_index = mapping[node.right.val]
+            q.append(node.right)
+        else:
+            right_index = -1
+
+        res.append([val, left_index, right_index])
+
+    return res
+
+
+def test_stress():
+
+    for _ in range(100):
+        my_tree = tree(height=2, is_perfect=False)
+        n = my_tree.size
+        tree_reformat = convert_tree_format(my_tree)
+
+        solver = Solver(n, tree_reformat)
+        solver2 = Solver2(n, tree_reformat)
+        if solver.is_bst() != solver2.is_bst():
+            print('n: ', n)
+            print('tree_reformat: ', tree_reformat)
